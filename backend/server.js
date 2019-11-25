9347be9f160fc664d8d498212985257f5aa6cbbd
Jozef Daxner <daxner.dodo@gmail.com>
2019-11-25 14:23:51 +0100

    add cryptoapis

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,14 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 var db_conf = require("./database_conf");
+var cryptoapis = require("./cryptoapis");
 
 var corsOptions = {
   origin: 'http://localhost:4200',
   optionSuccessStatus: 200
 }
+
+
 
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
@@ -16,14 +19,20 @@ app.use(cors(corsOptions));
 app.listen(8080, () => {
   console.log('Server started!');
 
+  //cryptoapis.createAccount();
+  //cryptoapis.getAssets(200);
+  //cryptoapis.getSpecRate('5b1ea92e584bf50020130637', '5b1ea92e584bf50020130670');
+  //cryptoapis.listAllAccounts();
+  //cryptoapis.getAllRates('5b1ea92e584bf50020130616');
+
   // select all from table
-  db_conf.db.any('SELECT * FROM transaction_type')
-    .then(function(data) {
-        console.log(data);
-      })
-      .catch(function(error) {
-        console.log(error);
-    });
+  // db_conf.db.any('SELECT * FROM transaction_type')
+  //   .then(function(data) {
+  //       console.log(data);
+  //     })
+  //     .catch(function(error) {
+  //       console.log(error);
+  //   });
 
   // insert into db example
   //db.none('INSERT INTO users(first_name, last_name, age) VALUES(${name.first}, $<name.last>, $/age/)', {
@@ -32,14 +41,15 @@ app.listen(8080, () => {
   //});
 });
 
-app.route('/api/test').get((req, res) => {
-  console.log('Request accepted!');
-  res.setHeader('Content-type', 'application/json');
-  res.send(JSON.stringify({
-    testMessages: 'fine'
-  }));
-});
+// app.route('/api/test').get((req, res) => {
+//   console.log('Request accepted!');
+//   res.setHeader('Content-type', 'application/json');
+//   res.send(JSON.stringify({
+//     testMessages: 'fine'
+//   }));
+// });
 
-app.route('/api/test').post((req, res) => {
-  console.log(req.body);
-});
+// app.route('/api/test').post((req, res) => {
+//   console.log(req.body);
+// });
+
