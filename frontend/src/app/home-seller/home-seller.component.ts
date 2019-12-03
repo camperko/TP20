002f76c15b2401be87763a44701af0d3aae1cf88
queen-Lenocka <lenocka.koplikova@gmail.com>
2019-12-03 17:16:02 +0100

    fixed frontend cosmetic issues registration and login form

--- frontend/src/app/home-seller/home-seller.component.ts
+++ frontend/src/app/home-seller/home-seller.component.ts
@@ -2,12 +2,9 @@
 This component is home page for logged user. Not to be mistaken for home component.
 - Lenočka
 */
-
 import { Component } from '@angular/core';
 import { first } from 'rxjs/operators';
 
-import { User } from '@app/_models';
-import { UserService, AuthenticationService } from '@app/_services';
 
 @Component({ templateUrl: 'home-seller.component.html' })
 export class HomeSellerComponent {
