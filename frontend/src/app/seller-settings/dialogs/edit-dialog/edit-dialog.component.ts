651b01f31dd8eaa5ed31ce73e13a417242144088
Jozef Daxner <daxner.dodo@gmail.com>
2020-04-09 15:29:14 +0200

    Reduce wallets of seller to one per transaction type.

--- frontend/src/app/seller-settings/dialogs/edit-dialog/edit-dialog.component.ts
+++ frontend/src/app/seller-settings/dialogs/edit-dialog/edit-dialog.component.ts
@@ -7,15 +7,26 @@ import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
   styleUrls: ['./edit-dialog.component.sass']
 })
 export class EditDialogComponent implements OnInit {
-
+  alreadyExists = false;
   currentWallet: any;
+  sellerWallets: any[];
   constructor(
       public dialogRef: MatDialogRef<EditDialogComponent>,
       @Inject(MAT_DIALOG_DATA) public data: any) {
     this.currentWallet = JSON.parse(JSON.stringify(data.wallet));
+    this.sellerWallets = JSON.parse(JSON.stringify(data.wallets));
+    this.sellerWallets = this.sellerWallets.filter(wallet => wallet.type_name !== this.currentWallet.type_name);
   }
 
   ngOnInit() {
+  }
+
+  checkExistingWallets() {
+    if (this.sellerWallets.find(wallet => wallet.type_name === this.currentWallet.type_name) !== undefined) {
+      this.alreadyExists = true;
+    } else {
+      this.alreadyExists = false;
+    }
   }
 
   onClose(): void {
