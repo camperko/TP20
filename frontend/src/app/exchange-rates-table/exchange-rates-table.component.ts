38b7894a9dcc8d18a064adbb9c2a2d8ba22264dc
Veronika Klaciková <veronikaklacikova@gmail.com>
2020-05-02 20:51:39 +0200

    edited exchange rate table to not display duplicates

--- frontend/src/app/exchange-rates-table/exchange-rates-table.component.ts
+++ frontend/src/app/exchange-rates-table/exchange-rates-table.component.ts
@@ -1,14 +1,20 @@
 /*
-Exchange rates table
-- gets asset details on init, then every 4 seconds
--
+  ExchangeRatesTableComponent - gets asset details (exchange rates) on init, then every 4 seconds
+  attributes:
+    - displayedColumns
+    - dataSource
+    - error
+  methods:
+    - ngOnInit
+    - getAssetDetails
 */
+
 import { Component, OnInit } from '@angular/core';
 import { ExchangeRatesTableService } from './exchange-rates-table.service';
 import { MatTableDataSource } from '@angular/material';
 
 export interface ExchangeRatesRow {
-  currency: string;
+  pair: string;
   price: string;
   change: string;
   high: string;
@@ -28,14 +34,29 @@ export class ExchangeRatesTableComponent implements OnInit {
   constructor(private exchangeRatesTableService: ExchangeRatesTableService) { }
 
   ngOnInit() {
+    this.dataSource.data = [];
     this.getAssetDetails();
     setInterval(() => this.getAssetDetails(), 4000);
   }
 
+  /*
+    getAssetDetails - void function
+      - get exchange rates and push new exchange pairs or replace existing exchange pairs in this.dataSource.data to prevent duplicates
+  */
   getAssetDetails() {
     this.exchangeRatesTableService.getAssetDetails().subscribe(
       data => {
-        this.dataSource = new MatTableDataSource<ExchangeRatesRow>(data.data);
+        for (var i = 0; i < data.data.length; i++) {
+          if (this.dataSource.data.filter(function(e) { return e.pair === data.data[i].pair; }).length <= 0) {
+            this.dataSource.data.push(data.data[i]);
+            this.dataSource.data = [...this.dataSource.data];
+          }
+          else {
+            let index = this.dataSource.data.map(function(e) { return e.pair; }).indexOf(data.data[i].pair);
+            this.dataSource.data[index] = data.data[i];
+            this.dataSource.data = [...this.dataSource.data];
+          }
+        }
       },
       error => this.error
     );
